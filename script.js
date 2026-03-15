884616d697b704ff8f5244caf6b20fe1be5f3990
kiran2128 <kiran953051@gmail.com>
2026-03-15 15:23:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// Newsletter subscribe alert
+// Cart ko initialize karein (purane items check karein ya khali array lein)
+let cart = JSON.parse(localStorage.getItem('myCart')) || [];
+
+function addToCart(name, price) {
+    // Har baar fresh data fetch karein
+    let cart = JSON.parse(localStorage.getItem('myCart')) || [];
+
+    // Item add karein
+    cart.push({ name: name, price: price });
+
+    // LocalStorage update
+    localStorage.setItem('myCart', JSON.stringify(cart));
+
+    alert(name + " added to cart!");
+    
+    // Direct cart page par bhejna
+    window.location.href = "cart.html";
+}
 
 function subscribe(){
-
-alert("Thank you for subscribing to our newsletter!");
-
+    alert("Thank you for subscribing to our newsletter!");
 }
